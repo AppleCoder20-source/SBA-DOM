f5c3f3fe74d86115743fd82ce6e3601d3e9ae474
Mashiur <56059603+AppleCoder20-source@users.noreply.github.com>
2024-10-22 20:09:55 -0400

    Finished adding ParentNode, FirstChild elements

--- Info.js
+++ Info.js
@@ -1,13 +1,25 @@
 const info = document.getElementById("Info")
-document.addEventListener("DOMContentLoaded", () => {
-    document.body.classList.add('custom-background1') 
-});
+document.addEventListener("DOMContentLoaded", handlePageLoad);
+
+function handlePageLoad() {
+    document.body.classList.add('custom-background1'); 
+}
 
 info.addEventListener("click",() =>{;
     Screens();
+    NodeMapping();
 });
+
 function Screens() {
     const screenWidth = screen.availWidth;
     const screenHeight = screen.availHeight;
     info.innerHTML = `Available Screen Height: ${screenHeight}px and Available Screen Width is ${screenWidth}`;
 }
+
+function NodeMapping() {
+    const Parent = info.parentNode //get div content entirely 
+    const FirstChild = Parent.firstElementChild
+    FirstChild.innerHTML = "<h4> Location Information has been sent successfully!</h4>";
+       
+}
+
